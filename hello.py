7dadf2a9c3c11f72fef3f3f5ca21f974536fb624
takurx <taku3wzr@gmail.com>
2020-09-07 15:43:51 +0900

    template, flask
    template, flask

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello World"
-    return name
+    name = "Hoge"
+    return render_template('hello.html', title='flask test', name=name)
 
 @app.route('/good')
 def good():
